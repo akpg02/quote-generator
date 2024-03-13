ae7fb26f1069b17262d9aafcac20d26f7e36c47f
Grace Akpan <opps4lrng@gmail.com>
2024-03-13 16:16:52 -0600

    added a spinner and conditional statment for category selection

--- src/components/generator/QuoteGenerator.jsx
+++ src/components/generator/QuoteGenerator.jsx
@@ -5,11 +5,13 @@ import Quote from "../quote/Quote";
 
 import { quotesList } from "../../data";
 import Category from "../category/Category";
+import Spinner from "../spinner/Spinner";
 
 function QuoteGenerator() {
   const [quotes, setQuotes] = useState([]);
   const [quote, setQuote] = useState(quotes[0] || "");
   const [category, setCategory] = useState("all");
+  const [showCategory, setShowCategory] = useState(true);
 
   const getRandomQuote = useCallback(() => {
     const randNum = Math.floor(Math.random() * quotes.length);
@@ -22,7 +24,7 @@ function QuoteGenerator() {
         method: "GET",
         url: `https://famous-quotes4.p.rapidapi.com/random?category=${category}&count=25`,
         params: {
-          token: "ipworld.info",
+          token: process.env.REACT_APP_API_TOKEN,
         },
         headers: {
           "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
@@ -33,7 +35,9 @@ function QuoteGenerator() {
       try {
         const response = await axios.request(options);
         setQuotes(response.data);
+        setShowCategory(true);
       } catch (error) {
+        setShowCategory(false);
         setQuotes(quotesList);
         console.log(error);
       }
@@ -54,8 +58,12 @@ function QuoteGenerator() {
     <div className="quotes-container">
       <h1>Thought of the Day</h1>
       <h3>Quotes</h3>
-      <Category category={category} onChange={handleChange} />
-      <Quote text={quote?.text} author={quote?.author} />
+      {showCategory && <Category category={category} onChange={handleChange} />}
+      {quote ? (
+        <Quote text={quote?.text} author={quote?.author} />
+      ) : (
+        <Spinner />
+      )}
       <div className="button-group">
         <button onClick={getRandomQuote}>Get Quote</button>
       </div>
